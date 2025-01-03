38595cd8029fac4efc6cc36aec329a771a7d6563
pankaj <pandeypankaj1986@gmail.com>
2025-01-03 13:41:31 +0530

    added variable

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami-id" {
+    type = string
+    default = "ami-0e2c8caa4b6378d8c"
+    description = "This is ami-id for EC2 instance"  
+}
+
+variable "dynamodb-id" {
+    type = string
+    default = "value"
+    description = "This is dynamodb"
+  
+}
